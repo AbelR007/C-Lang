e0efce513ac24418d1b66906813564cf1426d578
Abel Roy <abelroi007@gmail.com>
2022-01-15 22:07:55 +0530

    Initial Commit

---
+++ for_loops.c
@@ -0,0 +1,29 @@
+// For Loops
+// ----------------------------------------------------------------
+// Basic loops based on the 5 step process of initalization, condition and progress!
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    /*
+        LOOPS :
+    ------------------------------------
+    Step 1 : Initialization of the Loop
+    Step 2 : Condition
+    Step 3 : Body of Loop
+    Step 4 : Body End of Loop
+    Step 5 : Progress
+    Step 6 : Repeat [ 2 -> 3 -> 4 -> 5 ]
+
+    */
+    printf("For Loops :\n----------\n");
+    for (int i = 1; i <= 5; i++)
+    {
+        printf("Hi!\n");
+    }
+    printf("Prints the above sentence 5 times...");
+}
+
+// Code by Abel Roy
