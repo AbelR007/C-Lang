b8fe58ee0ad866d073b8d2002ca24a73ab730a92
Abel Roy <abelroi007@gmail.com>
2022-01-16 18:10:22 +0530

    Added printf and array commands

---
+++ basic_printf.c
@@ -0,0 +1,13 @@
+// CODE for printf() command
+// ----------------------------------------------------------------
+// Used for printing a command on the output screen
+
+#include<stdio.h>
+
+void main()
+{
+    printf("Hello, world!");
+    getchar();
+}
+
+// Code by Abel Roy //
