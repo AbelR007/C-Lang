c9bd931a8b20a80132a3a23ee97ba5970ee83f37
Abel Roy <abelroi007@gmail.com>
2022-01-18 12:16:14 +0530

    Added new codes v1.3

---
+++ Code Definitions/hoto_print_%.c
@@ -0,0 +1,20 @@
+// How to print % in C
+//----------------------------------------------------------------
+// Doubt by my Computer Engineering Professor [Prof Fr Dr A K George]
+
+#include<stdio.h>
+
+void main() // Main Function
+{
+    printf("printf(\"%%d\",c);"); // Simple but confusing
+    /*
+        CONCEPT :
+    ----------------------------------
+    %% => for showing the % percentage symbol
+    \" => for printing the " double quotes (without causing error)
+    Then, printing the letter "d"
+
+    */
+}
+
+// Code by Abel Roy //
