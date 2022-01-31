d5b2e734978074a7762c8b66a7edd1722de33fb9
Abel Roy <abelroi007@gmail.com>
2022-01-31 16:30:16 +0530

    New Files on the Grid System

---
+++ Code Definitions/grid/spoon.c
@@ -0,0 +1,20 @@
+// SPOON using "Grid system"
+// ----------------------------------------------------------------
+#include<stdio.h>
+
+void main()
+{
+    for (int r = 1; r <= 20; r++)
+    {
+        for (int c = 1; c <= 20; c++)
+        {
+            if ((c==15 && r>15)||(r==c && r<16)||(r==20 && c>15)||(c==20 && r>15))
+                printf("%2d",r);
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+// Code by Abel Roy //
