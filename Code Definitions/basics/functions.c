52a9d27886b05644a0fe387e02cf2817f4cf842b
Abel Roy <abelroi007@gmail.com>
2022-02-21 20:04:23 +0530

    Update 1.5.2 Functions with helpful comments

---
+++ Code Definitions/basics/functions.c
@@ -0,0 +1,21 @@
+// Functions [with helpful comments]
+// ----------------------------------------------------------------
+#include<stdio.h>
+
+// New function we used
+void new_function()
+{
+    printf("This is a function\n");
+}
+
+// Main function
+void main()
+{
+    // Random printf command
+    printf("Functions!\n");
+
+    // Declaring a function in the main function
+    new_function();
+}
+// ----------------------------------------------------------------
+// Code by Abel Roy //
