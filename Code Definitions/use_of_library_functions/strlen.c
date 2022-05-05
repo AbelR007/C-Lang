d86b77986683ed2c33d3efd8dde2e3fc041b0d56
AbelR007 <abelroi007@gmail.com>
2022-05-06 01:48:37 +0530

    3.3.10 Library Functions added

---
+++ Code Definitions/use_of_library_functions/strlen.c
@@ -0,0 +1,12 @@
+/* Calculating length of string */
+// =================================================
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[10] = "AbelRoy";
+    int len = strlen(str);
+    printf("Length of string: %d\n", len);
+}
+// ================================================
+/* Code by Abel Roy */
---
+++ Code Definitions/use_of_library_functions/strrev.c
@@ -0,0 +1,13 @@
+/* Reversing string using Strrev */
+// =================================================
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char str[50] = "Pallotti";
+   printf("The given string is =%s\n",str);
+   printf("After reversing string is =%s",strrev(str));
+   return 0;
+}
+// ================================================
+/* Code by Abel Roy */
