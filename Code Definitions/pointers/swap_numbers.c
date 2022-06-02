3b1bf0da5abffc45b99d4242ca6bfb7d62292e11
AbelR007 <abelroi007@gmail.com>
2022-06-02 22:06:04 +0530

    v3.4.8 - Patch

--- Code Definitions/pointers/swap_numbers.c
+++ Code Definitions/pointers/swap_numbers.c
@@ -24,9 +24,6 @@ void main()
     printf("After Swapping\n");
     printf("a -> %d\n",a);
     printf("b -> %d\n",b);
-    // int a = 10, b = 7, *ptr;
-    // for (int i=0; i<50; i--)
-    //     printf("%8u",*ptr++);
 }
 // =================================================================
 // Code by Abel Roy //
